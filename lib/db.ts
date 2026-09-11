ce0f30e0391ea15a1604f018d3192386b81af98e
Gabriel Martinez <martinegabriel47@gmail.com>
2026-09-11 16:09:38 -0500

    create data dicrectory before create database

--- lib/db.ts
+++ lib/db.ts
@@ -1,6 +1,13 @@
 import Database from "better-sqlite3";
+import fs from "fs";
+import path from "path";
 
-export const db = new Database("data/bot.db"); // se crea una instancia con la ruta del archivo SQLite
+const dataDir = path.join(process.cwd(), "data");
+// pregunta si existe la ruta
+if (!fs.existsSync(dataDir)) {
+  fs.mkdirSync(dataDir, { recursive: true });
+}
+export const db = new Database(path.join(dataDir, "bot.db")); // se crea una instancia con la ruta del archivo SQLite
 
 db.exec(
   // check (id = 1) en vez de autoincrement, si no cumple con el id, no se crea otro registro
